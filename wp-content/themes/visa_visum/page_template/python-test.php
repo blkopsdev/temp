788a7cc12f0dec94cc1b72f8b0a6313afcc7084a
blkopsdev <blkopsdev@gmail.com>
2020-03-19 02:01:57 +0000

    modified:   wp-content/themes/visa_visum/page_template/python-test.php

--- wp-content/themes/visa_visum/page_template/python-test.php
+++ wp-content/themes/visa_visum/page_template/python-test.php
@@ -13,6 +13,6 @@ get_header();
 <?php
     $command = escapeshellcmd('python3 /var/www/visa/russia_visa.py 6 en');
     $output = shell_exec($command);
-    var_dump($output);
+    print_r($output)
 ?>
 <?php get_footer(); ?>
